789d39834631823ccc4e7230cdd05d94ef0cf735
joaolucasdacruz <joaolucas@mecajun.com>
2018-04-16 00:20:26 -0300

    Update Entidades.cpp

--- Entidades.cpp
+++ Entidades.cpp
@@ -1,1 +1,48 @@
 #include "Entidades.hpp"
+
+#include<iostream>
+
+/*-----------------------------------------Métoos de Vocabulario controlado-----------------------------------------------------*/
+void VocabContr::setNome(string name)
+{
+    nome.setNome(name);
+}
+
+void VocabContr::setIdioma(string language)
+{
+    idioma.setIdioma(language);
+}
+
+void VocabContr::setData(string date)
+{
+    data.setData(date);
+}
+/*--------------------------------------Métoos e Termo--------------------------------------------------------------------------*/
+
+void Termo::setNome(string name)
+{
+    nome.setNome(name);
+}
+
+
+void Termo::setData(string date)
+{
+    nome.setNome(date);
+}
+
+void Termo::setClasse(string clas)
+{
+    classe.setClasse(clas);
+}
+
+/*-------------------------------------Metoos de Definicao----------------------------------------------------------------------*/
+
+void Definicao::setTexto(string text)
+{
+    texto.setTexto(text);
+}
+
+void Definicao::setData(string date)
+{
+    data.setData(date);
+}
