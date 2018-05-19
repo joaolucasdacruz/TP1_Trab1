b861163b4bf3cdabc61d8815023757faffc14690
joaolucasdacruz <joaolucas@mecajun.com>
2018-05-19 16:32:52 -0300

    Update Entidades.hpp

--- Entidades.hpp
+++ Entidades.hpp
@@ -1,71 +1,164 @@
-#include "Entidades.hpp"
-
+#ifndef ENTIDADES_HPP_INCLUDED
+#define ENTIDADES_HPP_INCLUDED
 #include<iostream>
+#include "Dominios.hpp"
 
+using namespace std;
 
+/*-------------------------------------------Classe Leitor----------------------------------------------------------------------*/
 
-void Leitor::nomeVsSenha(Nome nome,Senha senha) throw (invalid_argument)
+class Leitor /// Esta classe representa usuarios com permissao de acessar para ver
+/// vocabularios controlados, termos e suas definicoes
+///Objetos dessa classe armazenam dados de nome, sobrenome, Email e senha
 {
-    bool nomeNaSenha;
 
-    string name = nome.getNome();
-    string password = senha.getSenha();
+private:
 
-    nomeNaSenha = findName(password,name);
-    //cout << name<<endl;
-   // cout << password<<endl;
-    if(nomeNaSenha==true)
+    Nome nome;
+    Sobrenome sobrenome;
+    Email email;
+    Senha senha;
+
+    bool findName(string , string );
+
+public:
+
+    void setNome(const Nome &nomeSet)
+/// Armazena um objeto da classe de dominio Nome
+/// que contem o nome do usuario
     {
-        throw invalid_argument("\nA senha contém o nome\n");
-
+        nomeVsSenha(nomeSet,senha);
+        this->nome = nomeSet;
     }
-}
 
- //Esse método tem que sair daqui e ir para as entidades de usuário.
- bool Leitor::findName(string senha, string nomeS){
+    Nome getNome()
+/// retorna um objeto da classe de dominio Nome
+/// que contem o nome do usuario
+    {
+        return (nome);
+    }
 
-     const int tamNome = 20;
-     const int tamSenha = 8;
+    void setSobrenome(const Sobrenome &sobrenome)
+/// Armazena um objeto da classe de dominio Sobrenome
+/// que contem o sobrenomenome do usuario.
+    {
+        this->sobrenome = sobrenome;
+    }
 
-     char compara[tamNome];
-     int i=0,j=0;
+    Sobrenome getSobrenome() const
+/// retorna um objeto da classe de dominio Sobreome
+/// que contem o sobrenome do usuario
+    {
+        return (sobrenome);
+    }
 
-     for(i=0;i<8;i++)
-     {
-     if(senha[i] == nomeS[0])                //Se alguma letra da senha é igual à primeira letra do nome
-         {                                   //As próximas letras em sequência são comparaas, até ser encontrada uma diferença
-                                             //Ou até todos os demais caracteres terem sido comparados em sequência
-             while(senha[i+j] == nomeS[j] || i+j <tamSenha){
-                 compara[j]= nomeS[j];       // Os caracteres iguais são armazenaos
-                 j=j+1;
-                }
-             if(compara==nomeS) // Se o conjunto de caracteres iguais em sequência conseguir formar o nome
-                 return(true);  // Significa que a senha contém o nome.
-             else
-                 j=0;
-         }
-     }
-     return (false);
- }
-               
+    void setEmail(const Email &email)
+/// Armazena um objeto da classe de dominio Email
+/// que contem o Email do usuario.
+    {
+        this->email = email;
+    }
+
+    Email getEmail()
+/// retorna um objeto da classe de dominio Email
+/// que contem o Email do usuario
+    {
+        return (email);
+    }
+
+    void setSenha(const Senha &senhaSet)
+/// Armazena um objeto da classe de dominio Senha
+/// que contem a Senha do usuario
+    {
+        nomeVsSenha(nome,senhaSet);
+        this->senha = senhaSet;
+    }
+
+    Senha getSenha()
+/// retorna um objeto da classe de dominio Senha
+/// que contem a Senha do usuario
+    {
+        return (senha);
+    }
+
+    void nomeVsSenha(Nome,Senha) throw (invalid_argument);
+/// Verifica se a senha escolhida contem o nome do usuario
+};
+
+/*-----------------------------------------------Classe desenvolvedor-----------------------------------------------------------*/
+
+class Desenvolvedor: public Leitor /// Esta classe representa usuarios com permissao de acessar para ver
+/// vocabularios controlados, termos e suas definicoes assim como altera-las e criar mais termos
+///Essa classe herda os metodos publicos de Leitor e tambem armazena uma data de entrada do desenvolvedor
+{
+
+
+private:
+
+    Data data;
+
+public:
+
+    void setData(const Data data)
+/// Armazena um objeto que contem
+/// uma data de entrada
+    {
+        this->data = data;
+    }
+
+    Data getData()
+/// Retorna um objeto que contem
+/// uma data de entrada
+    {
+        return (data);
+    }
+
+};
+
+
+/*-------------------------------------------------Classe Administrador---------------------------------------------------------*/
+
+class Administrador: public Desenvolvedor /// Esta classe representa usuarios com permissao de acessar para ver
+/// vocabularios controlados, termos e suas definicoes assim como altera-las e criar mais termos alem de
+/// criar listas de vocabulario controlado e excluir termors
+///Essa classe herda os metodos publicos de Desenvolvedor e os objetos da classe tambem armazenam o telefone e enereco do usuario
+{
+
+ private:
+    Telefone telefone;
+    Endereco endereco;
+
+ public:
+
+    void setTelefone(const Telefone &telefone)
+///Armazena um objeto contendo o telefone do usuario
+    {
+        this->telefone = telefone;
+    }
 
     Telefone getTelefone()
+///retorna um objeto contendo o telefone do usuario
     {
-        return telefone;
+        return (telefone);
     }
 
-    void setEndereco(const Endereco endereco)
+    void setEndereco(const Endereco &endereco)
+///Armazena um objeto contendo o endereco do usuario
     {
         this->endereco = endereco;
     }
 
     Endereco getEndereco()
+///Retorna um objeto contendo o endereco do usuario
     {
-        return endereco;
+        return (endereco);
     }
 };
 
-class VocabContr
+
+/*--------------------------------------------Classe Vocabulário controlado-----------------------------------------------------*/
+class VocabContr /// Classe que representa um vocabulario controlado
+/// Armazena dados de nome, idioma e data do vocabulario controlado
 {
 
 private:
@@ -76,49 +169,53 @@ private:
 
 public:
 
-// Método construtor
-    VocabContr()
+    void setNome (const Nome &nome)
+/// Armazena um objeto da classe de dominio Nome
+/// que contem o nome do vocabulario
     {
-    nome.setNome("Joaosemnome");
-    data.setData("00/00/0000");
-    idioma.setIdioma("POR");    // Idioma padrão
-    }
-    void setNome(string);
-    void setIdioma(string);
-    void setData(string);
-
-    string getNome() throw (invalid_argument)
-    {
-        if( nome.getNome() == "Joaosemnome")
-        {
-            throw invalid_argument ("Vocabulario controlado nao teve o nome inicializado");
-        }
-        else
-        {
-            return(nome.getNome());
-        }
+        this->nome=nome;
     }
 
-    string getIdioma()
+    Nome getNome()
+/// Retorna um objeto da classe de dominio Nome.
+/// que contem o nome do vocabulario.
     {
-        return(idioma.getIdioma());
+        return(nome);
     }
 
-    string getData()
+    void setIdioma(const Idioma &idioma)
+/// Armazena um objeto da classe de dominio Idioma
+/// que contem o idioma do vocabulario
     {
-        if( data.getData() == "00/00/0000")
-        {
-            throw invalid_argument ("Vocabulario controlado nao teve a data  inicializada");
-        }
-        else
-        {
-            return(data.getData());
-        }
+        this->idioma=idioma;
     }
+
+    Idioma getIdioma()
+/// Retorna um objeto da classe de dominio Idioma
+/// que contem o idioma do vocabulario
+    {
+        return(idioma);
+    }
+
+    void setData (const Data &data)
+/// Armazena um objeto que contem
+/// uma data de entrada
+    {
+        this->data=data;
+    }
+
+    Data getData()
+/// Retorna um objeto que contem
+/// uma data de entrada
+    {
+        return(data);
+    }
+
 };
 /*---------------------------------------------------- Classe Termo-------------------------------------------------------------*/
 
-class Termo
+class Termo /// Esta classe representa um termo na lista de vocabulos em um vocabulario controlado
+/// Cada termo contem um nome, uma classe(preferido ou nao preferido) e uma data de entrada
 {
 private:
     Nome nome;
@@ -126,94 +223,92 @@ private:
     Data data;
 
 public:
-//Método construtor da classe termo que inicializa os atributos
-    Termo()
-    {// Essas inicializações significam que não foi usado nenhum set
-        nome.setNome("Joaosemnome");
-        classe.setClasse("PT");
-        data.setData("00/00/00");
-    }
 
-    void setNome(string);
-    void setClasse(string);
-    void setData(string);
-
-    string getNome()  throw (invalid_argument)
+    void setNome (const Nome &nome)
+/// Armazena um objeto da classe de dominio Nome
+/// que contem o nome do termo
     {
-        if(nome.getNome()=="Joaosemnome")
-        {
-            throw invalid_argument("Nome do termo nao foi inicializado");
-        }
-        else
-        {
-            return(nome.getNome());
-        }
+        this->nome=nome;
     }
 
-    string getData() throw (invalid_argument)
+    Nome getNome()
+/// Retorna um objeto da classe de dominio Nome
+/// que contem o nome do termo
     {
-        if(data.getData()=="00/00/0000")
-        {
-            throw invalid_argument("Data do termo nao inserida");
-        }
-        else
-        {
-            return(data.getData());
-        }
+        return(nome);
     }
 
-    string getClasse()
+    void setClasse(const ClasseDeTermo &classe)
+/// Armazena um objeto da classe de dominio Classe
+/// que informa se o termo eh ou nao preferido
     {
-        return(classe.getClasse());
+        this->classe=classe;
     }
 
+    ClasseDeTermo getClasse()
+/// Retorna um objeto da classe de dominio Classe
+/// que informa se o termo eh ou nao preferido
+    {
+        return(classe);
+    }
+
+    void setData (const Data &data)
+/// Armazena um objeto que contem
+/// uma data de entrada
+    {
+        this->data=data;
+    }
+
+    Data getData()
+/// Retorna um objeto que contem
+/// uma data de entrada
+    {
+        return(data);
+    }
 
 };
 
 /*---------------------------------------------------- Classe Definiçao --------------------------------------------------------*/
 
 
-class Definicao
+class Definicao /// Esta classe representa o significado de um termo
+/// E um objeto dessa classe deve ter o texto que explica o signficado e uma data de entrada do texto
 {
 
 private:
     Data data;
     TextoDef texto;
 public:
-    Definicao()
+
+
+    void setTexto (const TextoDef &texto)
+///Armazena um objeto que contem ou texto
+/// que define um termo
     {
-        data.setData("00/00/0000");
-        texto.setTexto("!@#$%&*");
+        this->texto=texto;
     }
 
-    void setData(string);
-    void setTexto(string);
-
-    string getData() throw (invalid_argument)
+    TextoDef getTexto()
+///Retorna um objeto que contem ou texto
+/// que define um termo
     {
-        if(data.getData()=="00/00/0000")
-        {
-            throw invalid_argument("Nenhuma data foi inserida para o texto");
-        }
-        else
-        {
-            return(data.getData());
-        }
+        return(texto);
     }
 
-    string getTexto() throw (invalid_argument)
+    void setData (const Data &data)
+/// Armazena um objeto que contem
+/// uma data de entrada
     {
-
-        if( texto.getTexto() == "------------")
-        {
-            throw invalid_argument("\n nenhum texto foi inserido no momento \n");
-        }
-        else
-        {
-        return(texto.getTexto());
-        }
+        this->data=data;
     }
+
+    Data getData()
+/// Retorna um objeto que contem
+/// uma data de entrada
+    {
+        return(data);
+    }
+
 };
-
 
 #endif
